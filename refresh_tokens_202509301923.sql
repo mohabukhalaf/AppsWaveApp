ecb5e96c7c53e2ff069473c3332334e6a71aebe2
mohabukhalaf <60516042+mohabukhalaf@users.noreply.github.com>
2025-09-30 21:53:37 +0300

    upload Postman collection file and DB dump

---
+++ refresh_tokens_202509301923.sql
@@ -0,0 +1,4 @@
+INSERT INTO mydb.refresh_tokens (expiry_date,revoked,token,user_id) VALUES
+	 ('2025-09-30 18:26:07.278451',0,'434b72a7-b238-4da0-a9c1-8e24691334b2',2),
+	 ('2025-09-30 18:26:55.641795',0,'8dd8342b-5575-4088-9f3b-0db180c71cfd',3),
+	 ('2025-09-30 19:30:06.913985',0,'cc1d013c-3c68-4804-bd92-a3b448b4606f',1);
---
+++ users_202509301923.sql
@@ -0,0 +1,4 @@
+INSERT INTO mydb.users (created_at,date_of_birth,email,full_name,password,`role`,updated_at) VALUES
+	 ('2025-09-30 03:52:54.461522','1998-03-01','mohammad@hotmail.com','mohammad abu khalaf','$2a$10$7rDcc3c1hHYSoWkDUCHJr.4pUZtHieQR2KQqWMa1exbt.UajJoYZm','ADMIN','2025-09-30 14:36:43.376242'),
+	 ('2025-09-30 14:38:10.098956','2025-09-30','abd@hotmail.com','abd','$2a$10$zlBRJvNIZkvAsl9dvkoNBubEWxDTINNnr7xVwCZm/hE7ZWdv6N8/S','NORMAL','2025-09-30 14:38:10.099945'),
+	 ('2025-09-30 14:48:42.577484','2025-09-30','samer@hotmail.com','Samer','$2a$10$0//78wPFe/iH.VDm.ZH7QOwrrGFtvIWmIxbr0IFB5GBVeUwVKcn5y','CONTENT_WRITER','2025-09-30 14:48:42.578489');
